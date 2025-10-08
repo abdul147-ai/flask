c72bdcc7d12d102b6e5f12fa36f93852f0a2e32a
abdul147-ai <abdulaziz047333@gmail.com>
2025-10-08 13:28:52 +0530

    post

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ class Posts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     slug = db.Column(db.String(21), nullable=False)
-    content = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.String(1200), nullable=False)
     tagline = db.Column(db.String(120), nullable=False)
     date = db.Column(db.DateTime, nullable=True)
     img_file = db.Column(db.String(20), nullable=True)
